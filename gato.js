348eccac7dc401295909c992d949945ce80831fd
cristian <cristianholguin00@gmail.com>
2018-07-04 19:48:38 -0500

    Gato

---
+++ gato.js
@@ -0,0 +1,21 @@
+var fabricaDeGatos = function (patas, colorPelaje) {
+    this.patas = patas;
+    this.colorPelaje = colorPelaje;
+
+    this.hablar = function (){
+    	alert("miauuu... miauuuu");
+    }
+}
+
+
+// Felix El Gato
+var felix = new fabricaDeGatos(2, "#000");
+felix.hablar = function() {
+	alert('Soy Felix');
+}
+
+felix.hablar();
+
+var garfield = new fabricaDeGatos(4, "#F37C22");
+
+garfield.hablar();
